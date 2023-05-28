7d51217b656b003ff56fa2940c6b5c3fb99918dd
Özgür Yildirim <ozguryenihayat@gmail.com>
2023-05-28 14:06:52 +0200

    function fill_recipes

--- blog.py
+++ blog.py
@@ -78,43 +78,48 @@ def main():
         initialize_database(cur)
         conn.commit()
 
-        print('Pass the empty recipe name to exit.')
-        while recipe_name := input('Recipe name: '):
-            recipe_description = input('Recipe description: ')
-            cur.execute(f"""INSERT INTO recipes (recipe_name, recipe_description) 
-                            VALUES ('{recipe_name}', '{recipe_description}');
-                            """)
-            recipe_id = cur.lastrowid
-
-            meals = cur.execute("SELECT * FROM meals;").fetchall()
-            meals_string = " ".join(") ".join(map(str, meal)) for meal in meals)
-            print(meals_string)
-            meals_choice = map(int, input("When can the dish be served: ").split())
-            for choice in meals_choice:
-                cur.execute(f"INSERT INTO serve (recipe_id, meal_id) VALUES ({recipe_id}, {choice});")
-
-            while quantity_ingredient := input('Input quantity of ingredient <press enter to stop>: '):
-                qmi_input = quantity_ingredient.split()
-                quantity = int(qmi_input[0])
-                measure = qmi_input[1] if len(qmi_input) == 3 else ''
-                ingredient = qmi_input[-1]
-                cur.execute(f"SELECT measure_id FROM measures WHERE measure_name LIKE '{measure + '%' if measure else ''}';")
-                if len((matching_measures := cur.fetchall())) == 1:
-                    measure_id = matching_measures[0][0]
-                else:
-                    print('The measure is not conclusive!')
-                    continue
-                cur.execute(f"SELECT ingredient_id FROM ingredients WHERE ingredient_name LIKE '%{ingredient}%';")
-                if len((matching_ingredients := cur.fetchall())) == 1:
-                    ingredient_id = matching_ingredients[0][0]
-                else:
-                    print('The ingredient is not conclusive!')
-                    continue
-                cur.execute(f"INSERT INTO quantity (quantity, measure_id, ingredient_id, recipe_id) "
-                            f"VALUES ({quantity}, {measure_id},{ingredient_id},{recipe_id});")
+        fill_recipes(cur)
 
     conn.commit()
     conn.close()
+
+
+def fill_recipes(cur):
+    print('Pass the empty recipe name to exit.')
+    while recipe_name := input('Recipe name: '):
+        recipe_description = input('Recipe description: ')
+        cur.execute(f"""INSERT INTO recipes (recipe_name, recipe_description) 
+                                VALUES ('{recipe_name}', '{recipe_description}');
+                                """)
+        recipe_id = cur.lastrowid
+
+        meals = cur.execute("SELECT * FROM meals;").fetchall()
+        meals_string = " ".join(") ".join(map(str, meal)) for meal in meals)
+        print(meals_string)
+        meals_choice = map(int, input("When can the dish be served: ").split())
+        for choice in meals_choice:
+            cur.execute(f"INSERT INTO serve (recipe_id, meal_id) VALUES ({recipe_id}, {choice});")
+
+        while quantity_ingredient := input('Input quantity of ingredient <press enter to stop>: '):
+            qmi_input = quantity_ingredient.split()
+            quantity = int(qmi_input[0])
+            measure = qmi_input[1] if len(qmi_input) == 3 else ''
+            ingredient = qmi_input[-1]
+            cur.execute(
+                f"SELECT measure_id FROM measures WHERE measure_name LIKE '{measure + '%' if measure else ''}';")
+            if len((matching_measures := cur.fetchall())) == 1:
+                measure_id = matching_measures[0][0]
+            else:
+                print('The measure is not conclusive!')
+                continue
+            cur.execute(f"SELECT ingredient_id FROM ingredients WHERE ingredient_name LIKE '%{ingredient}%';")
+            if len((matching_ingredients := cur.fetchall())) == 1:
+                ingredient_id = matching_ingredients[0][0]
+            else:
+                print('The ingredient is not conclusive!')
+                continue
+            cur.execute(f"INSERT INTO quantity (quantity, measure_id, ingredient_id, recipe_id) "
+                        f"VALUES ({quantity}, {measure_id},{ingredient_id},{recipe_id});")
 
 
 def initialize_database(cur):
